552bf4738e5a99ac6ae383b1c02eb8706a44e006
David Caballero <46259337+davidcbllr82@users.noreply.github.com>
2019-01-06 20:11:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,37 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
-//body.style.background = "red";
+var randomButton = document.querySelector("button");
 
 function setGradient() {
-	body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+	body.style.background =
+		"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
 	css.textContent = body.style.background + ";";
 }
- 
-color1.addEventListener("input", setGradient); 
 
-color2.addEventListener("input", setGradient); 
 
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function randomBackground() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+// Random button change
+
+randomButton.addEventListener("click", randomBackground);
